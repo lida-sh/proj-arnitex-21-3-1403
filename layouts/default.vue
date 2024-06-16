cb6ecfd09713a9b40e3bc25888fa2944f4ce7bf4
Mehdi <ghaderimahdi850@gmail.com>
2024-06-16 15:02:26 +0330

    hiddenFooterRoutes '/login', '/register'

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,9 +2,15 @@
     <div>
       <the-menu></the-menu>
       <slot></slot>
-      <!-- <the-footer></the-footer> -->
+      <the-footer v-if="showFooter"></the-footer>
     </div>
   </template>
-  <script lang="ts" setup>
   
-  </script>
+  <script lang="ts" setup>
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const hiddenFooterRoutes = ['/login', '/register'];
+
+const showFooter = !hiddenFooterRoutes.includes(route.path);
+</script>
