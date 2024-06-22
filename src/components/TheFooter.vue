d61573ed78425dbf40b98b04c35eb6ad5343aaab
mostafaa <nematpor46@gmail.com>
2024-06-22 15:37:59 +0330

    ticker position fixed

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,8 +1,6 @@
 <template>
-  <div class="bg-gradient-to-b from-[#262626] to-[#171717] w-full h-[36.938rem]">
-    <div
-      class="w-[95rem] mx-auto h-full grid grid-cols-14  z-10 px-[6.25rem] py-[3.125rem]"
-    >
+  <div class="bg-gradient-to-b from-[#262626] to-[#171717] w-full pb-4">
+    <div class="w-[95rem] mx-auto h-full grid grid-cols-14  z-10 px-[6.25rem] py-[3.125rem]">
       <div class="col-span-4">
         <div class="flex flex-col gap-10">
           <h3 class="text-2xl text-[#676767] font-normal">دسترسی سریع</h3>
@@ -51,26 +49,28 @@
       </div>
       <div class="col-span-full">
         <div class="flex items-center justify-between">
-            <h6 class="text-xs font-thin text-[#676767]">تمامی حقوق برای آرنیتکس محفوظ می باشد</h6>
-            <div class="flex items-center">
-                <div class="w-[7.25rem] h-[2.625rem] border-[#676767] border flex items-center justify-between px-4 py-2.5 rounded-md ml-3">
-                    <span class="text-base font-thin text-white">فارسی</span>
-                    <div class="h-5 w-5">
-                        <img src="~/assets/images/Vector.png" alt="" class="">
-                    </div>
-                </div>
-                <div class="w-[3.75rem] h-[1.875rem] border-l ml-[1.625rem] flex items-center justify-center">
-                    <img src="~/assets/images/moon.png" alt="" class="w-5 h-5">
-                </div>
-                <div class="w-30 h-4 flex items-center justify-center">
-                    <img src="~/assets/images/arnitex.png" alt="" class="">
-                </div>
-                <div class="h-16 w-[4.5rem] flex items-center justify-center">
-                    <img src="~/assets/images/logo.png" alt="" class="w-8 h-7">
-                </div>
+          <h6 class="text-xs font-thin text-[#676767]">تمامی حقوق برای آرنیتکس محفوظ می باشد</h6>
+          <div class="flex items-center">
+            <div
+              class="w-[7.25rem] h-[2.625rem] border-[#676767] border flex items-center justify-between px-4 py-2.5 rounded-md ml-3">
+              <span class="text-base font-thin text-white">فارسی</span>
+              <div class="h-5 w-5">
+                <img src="~/assets/images/Vector.png" alt="" class="">
+              </div>
             </div>
+            <div class="w-[3.75rem] h-[1.875rem] border-l ml-[1.625rem] flex items-center justify-center">
+              <img src="~/assets/images/moon.png" alt="" class="w-5 h-5">
+            </div>
+            <div class="w-30 h-4 flex items-center justify-center">
+              <img src="~/assets/images/arnitex.png" alt="" class="">
+            </div>
+            <div class="h-16 w-[4.5rem] flex items-center justify-center">
+              <img src="~/assets/images/logo.png" alt="" class="w-8 h-7">
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </div>
+  <br>
 </template>
