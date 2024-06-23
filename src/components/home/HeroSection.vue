7bdd955deb9e02d5e5763df0a13e87c3f1bb0ed6
lida-sh <l_shafiei_2005@yahoo.com>
2024-06-23 13:22:50 +0330

    fix herosection

--- src/components/home/HeroSection.vue
+++ src/components/home/HeroSection.vue
@@ -4,20 +4,23 @@
 <template>
   <section class="block">
 
-    <div class="w-screen lg:w-[95rem] lg:mx-auto">
-      <div class="relative w-full h-full lg:h-[60rem] lg:flex lg:justify-between lg:pr-24 lg:pl-[5.563rem] px-4 py-6">
+    <div class="w-screen max-w-[95rem] mx-auto">
+      <div
+        class="relative w-full h-auto gap-[1.104rem] lg:flex lg:justify-between lg:pr-24 lg:pl-[5.563rem] px-4 mx-auto  py-10 sm:max-w-[38rem] lg:max-w-full">
         <!-- <div class="absolute">
         <img src="~/assets/images/Frame.png" alt="" class="">
       </div> -->
-        <div class="flex flex-col z-10 relative">
-          <div class="flex flex-col gap-3 lg:gap-7 h-full w-full text-white items-start z-10 justify-center">
-            <div class="h-14 w-full text-[2.8125rem] lg:text-5xl font-black">
+        <div class="flex flex-col justify-center items-center z-10 relative ">
+          <div
+            class="flex flex-col gap-[0.563rem] lg:gap-7 h-full w-full text-white z-10 justify-center items-center xl:items-start">
+            <div class="text-[2.813rem] font-bold lg:text-5xl lg:font-black">
               صرافی آرنیتکس
             </div>
-            <div class="w-[20.5rem] h-32 text-[2rem] lg:text-4xl font-normal">
+            <div class="text-base lg:text-3xl font-bold lg:font-normal lg:w-[24rem]">
               بازار بین المللی خرید و فروش ارز دیجیتال
             </div>
-            <div class="w-[20.5rem] lg:w-[22.5rem] h-14 relative rounded-md overflow-hidden text-white text-base">
+            <div
+              class="w-[20.5rem] lg:w-[22.5rem] h-14 mt-[3.75rem] lg:mt-[1.75rem] relative rounded-md text-white text-base">
               <input type="text"
                 class="w-full h-full bg-[#262626] border-2 border-[#676767] focus:ring-0 focus:border-gray-600 text-white placeholder-white z-20"
                 placeholder="شماره همراه یا ایمیل" />
@@ -25,25 +28,42 @@
                 class="bg-[#FF7028] absolute left-0 top-0 h-full w-28 flex items-center justify-center rounded-md z-20">
                 ثبت نام
               </div>
+              <div class="absolute w-[34.375rem] h-[34.375rem] left-12 top-1/2 -translate-x-1/2 -translate-y-1/2 z-50">
+                <UiPulsShadow></UiPulsShadow>
+              </div>
             </div>
           </div>
-          <div class="absolute w-[34.375rem] h-[34.375rem] top-72 -left-52">
+          <!-- <div class="absolute w-[34.375rem] h-[34.375rem] top-48 -left-52">
             <UiPulsShadow></UiPulsShadow>
-          </div>
+          </div> -->
         </div>
         <!-- <div class="hidden lg:col-span-9">
       <div class="w-full h-auto relative"></div>
     </div> -->
-        <div class="flex items-center justify-end relative">
-          <img src="~/assets/images/123.png" alt="" class="hidden lg:block w-[46.938rem] h-[41.25rem] z-30" />
-          <img src="~/assets/images/Group 26086262.png" alt=""
-            class="lg:hidden block h-[11.80863rem] w-[11.80863rem]" />
-          <div class="absolute lg:w-full lg:h-full -top-10 -left-52 -z-1">
+        <!-- <div class="aspect-w-40 aspect-h-21 drop-shadow-lg filter blur-[4px]">
+        <figure class="overflow-hidden">
+          <img class="w-full h-full" loading="lazy" :src="data.src" />
+        </figure>
+      </div> -->
+        <div class="flex items-center justify-center mt-[1.104rem] lg:justify-end relative z-50">
+          <div class="aspect-video">
+            <figure class="">
+              <img src="~/assets/images/123.png" alt=""
+                class="relative w-[18.188rem] sm:w-full lg:w-[46.938rem] h-auto z-50" />
+            </figure>
+
+          </div>
+          <div class="absolute lg:w-full lg:h-full top-0 left-0 -z-1">
+            <UiPulsShadow></UiPulsShadow>
+          </div>
+          <!-- <img src="~/assets/images/Group 26086262.png" alt=""
+            class="lg:hidden block h-[11.80863rem] w-[11.80863rem]" />  -->
+          <!-- <div class="absolute lg:w-full lg:h-full top -left-52 -z-1">
             <UiPulsShadow></UiPulsShadow>
           </div>
           <div class="absolute lg:w-full lg:h-full -top-10 left-0 -z-1">
             <UiPulsShadow></UiPulsShadow>
-          </div>
+          </div> -->
         </div>
       </div>
     </div>
