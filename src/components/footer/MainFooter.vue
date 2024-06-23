475e9dffac60bfc90d47fc3378473ce37d4e5c50
mostafaa <nematpor46@gmail.com>
2024-06-23 11:25:23 +0330

    feat: footer responsive

--- src/components/footer/MainFooter.vue
+++ src/components/footer/MainFooter.vue
@@ -1,4 +1,7 @@
 <script setup>
+import FooterLang from '../icons/FooterLang.vue';
+
+
 const data = {
   quickAccess: {
     title: "دسترسی سریع",
@@ -109,42 +112,37 @@ const data = {
         },
       ]
   },
-
 }
 </script>
 
 <template>
   <footer class="bg-gradient-to-b z-[49] relative from-[#262626] to-[#171717] w-full pb-4">
-    <div class="w-full max-w-[95rem] mx-auto h-full z-10 px-[6.25rem] py-[3.125rem]">
+    <div class="w-full mx-auto h-full z-10 px-[1rem] lg:px-[2rem] xl:px-[4.25rem] 2xl:px-[6.25rem] max-w-[25rem] lg:max-w-[95rem] py-[3.125rem]">
       <FooterNav :data="data" class="hidden lg:block"></FooterNav>
       <FooterNavMob :data="data" class="lg:hidden block"></FooterNavMob>
 
-      <div class="flex flex-col-reverse lg:flex-row items-center justify-between mt-24">
-        <h6 class="text-xs font-normal text-[#676767]">تمامی حقوق برای آرنیتکس محفوظ می باشد</h6>
-        <div class="flex flex-col items-center">
+      <div class="flex flex-col-reverse lg:flex-row items-center justify-between mt-[1.875rem] lg:mt-24">
+        <h6 class="text-xs leading-[1.375rem] font-normal text-[#676767]">تمامی حقوق برای آرنیتکس محفوظ می باشد</h6>
 
+        <div class="flex flex-col-reverse lg:flex-row items-center mb-[3.125rem] lg:mb-0">
           <div class="flex items-center">
             <div
               class="w-[7.25rem] h-[2.625rem] border-[#676767] border flex items-center justify-between px-4 py-2.5 rounded-md ml-3">
               <span class="text-base font-thin text-white">فارسی</span>
-              <div class="h-5 w-5">
-                <img src="~/assets/images/Vector.png" alt="" class="">
+              <div class="w-5">
+                <FooterLang></FooterLang>
               </div>
             </div>
-
-            <div class="w-[3.75rem] h-[1.875rem] border-none  lg:border-l ml-[1.625rem] flex items-center justify-center">
-              <img src="~/assets/images/moon.png" alt="" class="w-5 h-5">
+            <div
+              class="w-[3.75rem] h-[1.875rem] border-0 lg:border-l ml-0 lg:ml-[1.625rem] flex items-center justify-center">
+              <UiThemMoonFooter></UiThemMoonFooter>
             </div>
           </div>
 
-          <div class="flex items-center">
-            <div class="w-30 h-4 flex items-center justify-center">
-              <img src="~/assets/images/arnitex.png" alt="" class="">
-            </div>
-            <div class="h-16 w-[4.5rem] flex items-center justify-center">
-              <img src="~/assets/images/logo.png" alt="" class="w-8 h-7">
-            </div>
-          </div>
+          <NuxtLink to="/" class="w-30 flex items-center h-auto justify-center mb-[2rem] lg:mb-0">
+            <IconsFooterLogo></IconsFooterLogo>
+          </NuxtLink>
+
         </div>
       </div>
     </div>
---
+++ src/components/icons/FooterCollapseArrowDown.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="10" height="5" viewBox="0 0 10 5" fill="none">
+    <path d="M1 1L4.94667 4L8.89333 1" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+  </svg>
+</template>
