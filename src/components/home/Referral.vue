362ce5ca8b143e906aa3f05c7e372b3c7d0a069b
mostafaa <nematpor46@gmail.com>
2024-06-24 09:36:36 +0330

    responsive Referral component

--- src/components/home/Referral.vue
+++ src/components/home/Referral.vue
@@ -1,11 +1,11 @@
 <script setup></script>
 
 <template>
-  <section class="block">
-    <div class="flex justify-between w-[95rem] h-[38.25rem] pr-[6.25rem] pl-[4.81rem] mx-auto">
-      <div class="flex flex-col justify-center items-start w-auto h-full  text-white">
-        <h2 class="text-[2.25rem] font-normal mb-7">با دعوت از دوستان خود کسب درآمد کنید</h2>
-        <h6 class="text[1.375rem] font-semibold mb-9">
+  <section class="block mb-[6.25rem] xl:mb-0">
+    <div class="flex flex-col-reverse xl:flex-row justify-between max-w-[95rem] xl:h-[38.25rem] pr-[6.25rem] pl-[4.81rem] mx-auto">
+      <div class="flex flex-col justify-center text-center xl:text-align-unset items-center w-auto xl:items-start h-full text-white">
+        <h2 class="text-[2.25rem] leading-[4.063rem] font-normal mb-[0.938rem] xl:mb-7">با دعوت از دوستان خود کسب درآمد کنید</h2>
+        <h6 class="text[1.375rem] leading-[2.313rem] font-semibold mb-[3.438rem] xl:mb-9">
           <span>با هر معامله دوستان معرفی شده ی شما به آرنیتکس</span>
           <span class="text-[#FF7028]"> ۲۵٪ پاداش </span>
           <span class="">از کارمزدشان به شما داده خواهد شد .</span>
@@ -18,7 +18,7 @@
       </div>
       <div class="flex items-center justify-center relative w-auto h-full aspect-video">
         <img src="~/assets/images/Group 26086268.png" alt="" class="w-[24.6875rem] h-auto relative z-20" />
-        <div class="absolute top-[10%] right-0  h-[34.375rem] w-[34.375rem]">
+        <div class="absolute translate-[50%,-50%] xl:translate-[unset] xl:top-[10%] xl:right-0 h-[34.375rem] w-[34.375rem] pointer-events-none">
           <UiPulsShadow></UiPulsShadow>
         </div>
       </div>
