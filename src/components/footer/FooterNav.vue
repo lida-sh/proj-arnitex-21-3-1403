56268d6a8773003cc63068289316f3d3a12fa2f2
mostafaa <nematpor46@gmail.com>
2024-06-26 16:48:43 +0330

    fix footer link

--- src/components/footer/FooterNav.vue
+++ src/components/footer/FooterNav.vue
@@ -6,7 +6,7 @@ const props = defineProps(['data'])
   <div>
     <div class="grid grid-cols-12">
       <div class="col-span-3">
-        <div class="flex flex-col gap-10">
+        <div class="flex flex-col gap-10 items-start">
           <h3 class="text-base text-[#676767] font-bold">{{ data.quickAccess.title }}</h3>
           <NuxtLink :to="item.path" class="text-sm hover:text-[#FF7028] text-white font-normal"
             v-for="item in data.quickAccess.list" :key="item.title">{{ item.title }}</NuxtLink>
@@ -14,21 +14,21 @@ const props = defineProps(['data'])
         </div>
       </div>
       <div class="col-span-3">
-        <div class="flex flex-col gap-10">
+        <div class="flex flex-col gap-10 items-start">
           <h3 class="text-base text-[#676767] font-bold">{{ data.info.title }}</h3>
           <NuxtLink :to="item.path" class="text-sm hover:text-[#FF7028] text-white font-normal"
             v-for="item in data.info.list" :key="item.title">{{ item.title }}</NuxtLink>
         </div>
       </div>
       <div class="col-span-3">
-        <div class="flex flex-col gap-10">
+        <div class="flex flex-col gap-10 items-start">
           <h3 class="text-base text-[#676767] font-bold">{{ data.academy.title }}</h3>
           <NuxtLink :to="item.path" class="text-sm hover:text-[#FF7028] text-white font-normal"
             v-for="item in data.academy.list" :key="item.title">{{ item.title }}</NuxtLink>
         </div>
       </div>
       <div class="col-span-3">
-        <div class="flex flex-col gap-10">
+        <div class="flex flex-col gap-10 items-start">
           <h3 class="text-base text-[#676767] font-bold">{{ data.contact.title }}</h3>
           <NuxtLink :to="item.path" class="text-sm hover:text-[#FF7028] text-white font-normal"
             v-for="item in data.contact.list" :key="item.title">{{ item.title }}</NuxtLink>
