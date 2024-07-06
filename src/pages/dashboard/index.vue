4412682790f35cda6a027d68aa12797a57ce56a1
Mehdi <ghaderimahdi850@gmail.com>
2024-07-06 10:31:06 +0330

    update new chart

--- src/pages/dashboard/index.vue
+++ src/pages/dashboard/index.vue
@@ -3,12 +3,13 @@
         <div class="w-full mx-6 flex flex-col rounded-lg bg-[#171717] pr-[2rem] pt-[0.625rem] pb-[0.875rem]" >
             <span class="text-white text-[16px] w-[118px] ">نمودار روند دارایی</span>
             <div class="w-full h-auto flex justify-center items-center">
-                <!-- <DashboardChartJs></DashboardChartJs> -->
+                 
                 <!-- <UiChartPieChart></UiChartPieChart>  -->
                  
             </div>
             
         </div>
+        <DashboardChartJs></DashboardChartJs>
         <div class="w-full px-4">
             <DashboardUserInfo></DashboardUserInfo>
                         <DashboardInviteFriends></DashboardInviteFriends>
