fac6df775e19fb8d98b273191b9943a5f3f4f5a0
Mehdi <ghaderimahdi850@gmail.com>
2024-07-07 12:06:49 +0330

    fix code

--- src/pages/dashboard/index.vue
+++ src/pages/dashboard/index.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="w-full h-full">
-        <div class="w-full mx-6 flex flex-col rounded-lg bg-[#171717] pr-[2rem] pt-[0.625rem] pb-[0.875rem]" >
+        <div class="w-full mx-6 flex flex-col rounded-lg bg-[#171717] pr-[2rem] pt-[0.625rem] pb-[0.875rem]">
             <span class="text-white text-[16px] w-[118px] ">نمودار روند دارایی</span>
             <div class="w-full h-auto flex justify-center items-center">
-                 
+
                 <!-- <UiChartPieChart></UiChartPieChart>  -->
-                 
+
             </div>
-            
+
         </div>
         <!-- <DashboardChartJs></DashboardChartJs> -->
 
@@ -15,15 +15,16 @@
         <div class="w-full px-4">
             <!-- <DashboardUserInfo></DashboardUserInfo> -->
             <!-- <DashboardInviteFriends></DashboardInviteFriends> -->
-            <DashboardTotalAssets></DashboardTotalAssets>
+            <!-- <DashboardTotalAssets></DashboardTotalAssets> -->
+            <DashboardArticles></DashboardArticles>
 
         </div>
-        
-        
+
+
     </div>
 </template>
 <script>
 definePageMeta({
-  layout: "dashboard",
+    layout: "dashboard",
 });
 </script>
