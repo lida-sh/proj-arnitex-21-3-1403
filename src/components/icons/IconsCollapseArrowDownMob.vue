2a9a2ca399883717ecb67c0b1c7b07c3485e074c
lida-sh <l_shafiei_2005@yahoo.com>
2024-07-08 13:07:58 +0330

    bug stepper fix

---
+++ src/components/icons/IconsCollapseArrowDownMob.vue
@@ -0,0 +1,10 @@
+<template>
+    <svg width="13" height="6" viewBox="0 0 13 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1 1.05383L6.5 4.94616L12 1.05383" stroke="white" stroke-width="2" stroke-linecap="round"
+            stroke-linejoin="round" />
+    </svg>
+
+
+</template>
+<script>
+</script>
---
+++ src/components/icons/IconsCollapseArrowUpMob.vue
@@ -0,0 +1,9 @@
+<template>
+    <svg width="13" height="7" viewBox="0 0 13 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M12 5.41724L6.5 1.52491L1 5.41724" stroke="white" stroke-width="2" stroke-linecap="round"
+            stroke-linejoin="round" />
+    </svg>
+
+</template>
+<script>
+</script>
