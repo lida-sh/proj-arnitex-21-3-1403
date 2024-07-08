251848ff3db1346ad91bbba6773923125cab8bbf
mostafaa <nematpor46@gmail.com>
2024-07-08 13:13:55 +0330

    refactor dashboard loading bar

--- src/layouts/dashboard.vue
+++ src/layouts/dashboard.vue
@@ -1,18 +1,18 @@
 <template>
-	<HeaderSticky></HeaderSticky>
-	<div class="pt-[54px] lg:pt-[63px] relative w-full">
-		<div v-if="pageLoading"
-			class="bg-black bg-opacity-70 fixed inset-0 h-full w-full flex items-center justify-center overflow-hidden z-[201]">
-			<UiLoadingSpinner></UiLoadingSpinner>
-		</div>
-		<NuxtPage class="z-1 relative"></NuxtPage>
-	</div>
-	<FooterMainFooter></FooterMainFooter>
+  <HeaderSticky></HeaderSticky>
+  <div class=" relative w-full">
+    <div v-if="pageLoading"
+      class="bg-black bg-opacity-70 fixed inset-0 h-full w-full flex items-center justify-center overflow-hidden z-[5000]">
+      <UiLoadingSpinner></UiLoadingSpinner>
+    </div>
+    <NuxtPage class="z-1 relative"></NuxtPage>
+  </div>
+  <FooterMainFooter></FooterMainFooter>
 </template>
 
 <script setup>
 const pageLoading = ref(true)
 onMounted(() => {
-	pageLoading.value = false
+  pageLoading.value = false
 })
 </script>
