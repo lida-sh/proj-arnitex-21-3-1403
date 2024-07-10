0be7cab6e3fc1314c8e7229b7fc12ba09e3c74a9
lida-sh <l_shafiei_2005@yahoo.com>
2024-07-10 16:57:04 +0330

    deposit id dashboard

---
+++ src/components/ui/Stepper.vue
@@ -0,0 +1,52 @@
+<template>
+
+
+    <!-- <ol class="relative text-white text-xs font-medium leading-5 border-s border-[#FF9A67] ">
+        <li v-for="(item, index) in list" class="mb-10 ms-6">
+            <span
+                class="absolute flex items-center justify-center w-[1.188rem] h-[1.188rem] bg-[#FF9A67] rounded-full -start-[10px] lsdt:border-s-0 ">
+                <template v-if="$slots.icon">
+                    <slot name="icon"></slot>
+                </template> -->
+    <!-- <template v-if="numberStep">
+                    {{index+1}}
+                </template>
+            </span>
+            {{ item.title }}
+        </li>
+    </ol> -->
+    <ul class="steps steps-vertical">
+        <li v-for="(item, index) in list" class="step step-primary text-white text-justify text-xs font-medium leading-5 justify-start" :class="{'counter':!numberStep}">{{ item.title }}</li>
+        
+    </ul>
+
+</template>
+<script setup lang="ts">
+interface ListItem {
+    title: string;
+}
+const props = defineProps<{ list: ListItem[], numberStep: boolean }>()
+</script>
+<style scoped>
+.step{
+    text-align: start;
+    
+}
+.step::after{
+    background-color: #FF9A67;
+    height: 1.188rem;
+    width: 1.188rem;
+    color: #FF9A67;
+    
+   
+   
+}
+
+.steps .step-primary::before{
+    background-color: #FF9A67;
+}
+.steps-vertical .step:before{
+    width: 1px;
+    margin-inline-start:47%
+}
+</style>
