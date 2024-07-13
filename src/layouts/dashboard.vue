1ef302b71de432a5c2f146b737887f03ffe704a6
Mehdi <ghaderimahdi850@gmail.com>
2024-07-13 16:07:17 +0330

    fix code

--- src/layouts/dashboard.vue
+++ src/layouts/dashboard.vue
@@ -10,7 +10,7 @@
       <div class="w-full max-w-[100rem] pt-[5rem] mx-auto gap-2 2xl:gap-[1.25rem] px-2 flex items-start pb-20">
         <DashboardMenuBar class="hidden xl:block sticky top-20 max-w-[16.5rem] min-w-[13rem] dashboard-card w-full">
         </DashboardMenuBar>
-        <main>
+        <main class="flex-1">
           <NuxtPage></NuxtPage>
         </main>
 
