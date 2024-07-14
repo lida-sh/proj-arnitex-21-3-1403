79fd591c5bb41670f0d826590abd21e6cb5b1c9e
lida-sh <l_shafiei_2005@yahoo.com>
2024-07-14 09:33:21 +0330

    deposit page with dashboard layout

--- src/components/dashboard/DepositDesktop.vue
+++ src/components/dashboard/DepositDesktop.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="w-full bg-black">
-        <div class="w-full max-w-[100rem] pt-[5rem] mx-auto gap-2 2xl:gap-[1.25rem] px-2 flex items-start">
-            <DashboardMenuBar class="max-w-[16.5rem] min-w-[13rem] dashboard-card w-full"></DashboardMenuBar>
-            <main class="flex flex-col flex-1 gap-2 2xl:gap-[1.25rem]">
+        <!-- <div class="w-full max-w-[100rem] pt-[5rem] mx-auto gap-2 2xl:gap-[1.25rem] px-2 flex items-start"> -->
+            <!-- <DashboardMenuBar class="max-w-[16.5rem] min-w-[13rem] dashboard-card w-full"></DashboardMenuBar> -->
+            <div class="flex flex-col flex-1 gap-2 2xl:gap-[1.25rem]">
                 <div class="w-[25.313rem]">
                     <UiButtonTabButtonDashboard class="flex-1 flex-row-reverse" :list="data" v-model="TabIndex">
                     </UiButtonTabButtonDashboard>
@@ -212,8 +212,8 @@
                     </div>
                 </div>
 
-            </main>
-        </div>
+            </div>
+        <!-- </div> -->
 
     </div>
 </template>
