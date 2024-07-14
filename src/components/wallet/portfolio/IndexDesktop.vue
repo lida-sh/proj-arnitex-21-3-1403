398bcf604857715734043bc4670bc3a179e7568a
Mehdi <ghaderimahdi850@gmail.com>
2024-07-14 13:02:49 +0330

    new page fix code

---
+++ src/components/wallet/portfolio/IndexDesktop.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class=" pb-7 border-b border-[#343434]">
+        <div class=" px-4">
+            <UiButtonTabButtonDashboard class="flex-1" :list="data" v-model="TabIndex">
+            </UiButtonTabButtonDashboard>
+        </div>
+    </div>
+
+    <WalletPortfolio></WalletPortfolio>
+    <!-- <WalletWithdrawalCryptocurrency v-if="TabIndex == 0"></WalletWithdrawalCryptocurrency> -->
+</template>
+<script setup>
+const TabIndex = ref(0)
+
+const data = ref([
+    {
+        id: 1,
+        title: "برداشت رمز ارز"
+    }, {
+        id: 2,
+        title: "برداشت تومان"
+    },
+])
+</script>
+<style lang="">
+
+</style>
