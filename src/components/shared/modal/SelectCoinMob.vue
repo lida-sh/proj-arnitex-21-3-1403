500c48f174d2b3c688fd1977f1d77e1060c579e3
mostafaa <nematpor46@gmail.com>
2024-07-20 11:10:36 +0330

    fix select coin mob modal

--- src/components/shared/modal/SelectCoinMob.vue
+++ src/components/shared/modal/SelectCoinMob.vue
@@ -54,21 +54,7 @@ watch(showModal, (newVal) => {
 
 
 const coinList = ref([
-{
-    name: 'بیت کوین',
-    symbol: 'BTC',
-    image: '/bitcoin-logo.png',
-  },
   {
-    name: 'بیت کوین',
-    symbol: 'BTC',
-    image: '/bitcoin-logo.png',
-  },
-  {
-    name: 'بیت کوین',
-    symbol: 'BTC',
-    image: '/bitcoin-logo.png',
-  },  {
     name: 'بیت کوین',
     symbol: 'BTC',
     image: '/bitcoin-logo.png',
@@ -82,7 +68,21 @@ const coinList = ref([
     name: 'بیت کوین',
     symbol: 'BTC',
     image: '/bitcoin-logo.png',
-  },  {
+  }, {
+    name: 'بیت کوین',
+    symbol: 'BTC',
+    image: '/bitcoin-logo.png',
+  },
+  {
+    name: 'بیت کوین',
+    symbol: 'BTC',
+    image: '/bitcoin-logo.png',
+  },
+  {
+    name: 'بیت کوین',
+    symbol: 'BTC',
+    image: '/bitcoin-logo.png',
+  }, {
     name: 'بیت کوین',
     symbol: 'BTC',
     image: '/bitcoin-logo.png',
@@ -120,12 +120,13 @@ const path = ref('/bitcoin-logo.png')
         <div class="flex flex-col mt-7 gap-4">
           <div class="relative">
             <input type="text" placeholder="جستجو رمز ارز"
-              class="pl-12 w-full bg-transparent rounded-lg border-1 border-white ps-3 text-white text-xs leading-6 font-medium placeholder:text-white py-2 px-1">
+              class="search-input pl-12 w-full bg-transparent rounded-lg border-1 border-[#A8A8A8] ps-3 text-white text-xs leading-6 font-medium placeholder:text-[#A8A8A8] focus:border-white py-2 px-1">
             <IconsSharedSelectCoinSearchMob class="absolute top-1/2 left-3 transform -translate-y-1/2">
             </IconsSharedSelectCoinSearchMob>
           </div>
           <ul class="max-h-64 overflow-auto">
-            <li v-for="item in coinList" :key="item" class="flex items-center hover:bg-[#000000] px-2 gap-4 border-solid border-b-[1px] border-[#BBBBBB]">
+            <li v-for="item in coinList" :key="item"
+              class="flex items-center hover:bg-[#000000] px-2 gap-4 border-solid border-b-[1px] border-[#BBBBBB]">
               <img class="w-10 h-10 object-contain" :src="item.image" alt="">
               <div class="flex flex-col items-start flex-1">
                 <span class="text-white font-normal text-base leading-7">{{ item.name }}</span>
@@ -171,5 +172,12 @@ const path = ref('/bitcoin-logo.png')
 .nested-leave-to .inner {
   transform: translateY(100%);
   opacity: 0.001;
+}
+
+.search-input:focus {
+  border: 1px solid #ffffff;
+  --tw-ring-offset-shadow: var(--tw-ring-inset) 0 0 0 var(--tw-ring-offset-width) var(--tw-ring-offset-color);
+  --tw-ring-shadow: var(--tw-ring-inset) 0 0 0 calc(0px + var(--tw-ring-offset-width)) var(--tw-ring-color);
+  box-shadow: var(--tw-ring-offset-shadow), var(--tw-ring-shadow), var(--tw-shadow, 0 0 #0000);
 }
 </style>
