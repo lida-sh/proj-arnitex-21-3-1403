c70cbd0978acb1ad886ef809757eec98a401cea0
mostafaa <nematpor46@gmail.com>
2024-07-21 09:02:10 +0330

    add orders-page

---
+++ src/pages/orders.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+definePageMeta({
+  layout: "dashboard",
+});
+
+useHead({
+  title: 'آرنیتکس | دارایی ها',
+})
+</script>
+
+<template>
+  <div class="bg-black">
+    <UiDatePickerSimple></UiDatePickerSimple>
+  </div>
+</template>
+
+<style scoped></style>
